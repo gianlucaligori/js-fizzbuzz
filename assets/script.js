7ceed8581374d6b656ad5fa29b15cf36da0e2b3b
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-04-14 17:27:25 +0200

    style added to fizz, buzz, fizzbuzz squares

--- assets/script.js
+++ assets/script.js
@@ -4,23 +4,22 @@ for (let i = 1; i <= 100; i++) {
 
 	if (i % 15 == 0) {
 		console.log('fizzbuzz');
-		eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">fizzbuzz</div>`;
+		eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell cell15">fizzbuzz</div>`;
 	}
 	
 	else if (i % 5 == 0) {
 		console.log('buzz');
-		eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">buzz</div>`;
+		eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell cell5">buzz</div>`;
 
 	}
 	
 	else if (i % 3 == 0) {
 		console.log('fizz');
-		eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">fizz</div>`;
-
+		eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell cell3">fizz</div>`;
 	}
 	
 	else   {
-		console.log(i);
+		console.log('i');
 		eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
 	}
 } 
