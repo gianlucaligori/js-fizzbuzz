56ad60831fd4b5ae916a46207dd2d04cf8adf89c
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-04-14 16:53:23 +0200

    add fizz buzz

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,28 @@
 const eleGrid = document.querySelector('.grid');
 
-for (let i = 0; i <= 100; i++) {
-	console.log(i);
-	eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+for (let i = 1; i <= 100; i++) {
+
+	if (i % 15 == 0) {
+		console.log('fizzbuzz');
+		eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">fizzbuzz</div>`;
+	}
+	
+	else if (i % 5 == 0) {
+		console.log('buzz');
+		eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">buzz</div>`;
+
+	}
+	
+	else if (i % 3 == 0) {
+		console.log('fizz');
+		eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">fizz</div>`;
+
+	}
+	
+	else   {
+		console.log(i);
+		eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+	}
 } 
+
+
